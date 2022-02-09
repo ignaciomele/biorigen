f9194b7dc8611ea399e05399cde12399c272a181
Nacho Mele <ignacio.mele@hotmail.com>
2022-02-09 13:16:30 -0300

    hook faqs

--- src/components/navbar/Fq/Fq.jsx
+++ src/components/navbar/Fq/Fq.jsx
@@ -1,24 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import Navbar from '../../../components/navbar/Navbar'
 import SocialFollow from '../../../components/socialFollow/SocialFollow'
 
-export default class Fq extends Component {
-    render() {
-        return (
-            <div>
-                <Navbar/>
-                <h1>Preguntas Frecuentes</h1>
-                <h3>¿Cómo solicito un turno?</h3>
-                <h3>¿En qué horarios puedo solicitarlo?</h3>
-                <h3>¿Dónde estan ubicados?</h3>
-                <h3>¿Con qué terapias trabajan?</h3>
-                <h3>¿Cómo encuentro al profesional que estoy buscando?</h3>
-                <h3>¿Tienen redes sociales?</h3>
 
-                <section>
+export default function BiorigenLikes() {
+    const [count, setCount] = useState(0)
 
-                </section>
-            </div>
-        )
-    }
+    return(
+        <div>
+            Likes: {count}
+            <br />
+            <button onClick={() => setCount(count + 1)}> LIKE</button>
+            <button onClick={() => setCount(count - 1)}> UNLIKE</button>
+        </div>
+    )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default class Fq extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <Navbar/>
+//                 <h1>Preguntas Frecuentes</h1>
+//                 <h3>¿Cómo solicito un turno?</h3>
+//                 <h3>¿En qué horarios puedo solicitarlo?</h3>
+//                 <h3>¿Dónde estan ubicados?</h3>
+//                 <h3>¿Con qué terapias trabajan?</h3>
+//                 <h3>¿Cómo encuentro al profesional que estoy buscando?</h3>
+//                 <h3>¿Tienen redes sociales?</h3>
+
+//                 <section>
+
+//                 </section>
+//             </div>
+//         )
+//     }
+// }
